8f327c4289ec2464a9c5da155cd098eac118f278
Dipak <dipak_pd@outlook.com>
2022-11-09 12:33:20 +0530

    Initial Commit

---
+++ FileZilla/FileZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileZillaApp.swift
+//  FileZilla
+//
+//  Created by Codebucket on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct FileZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
